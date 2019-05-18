5db2051639a98840834be8e3b6875828c5325f56
Salma Azzahra Salsabila <salma.2000@students.amikom.ac.id>
2019-05-18 15:09:22 +0700

    inheritance2

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person();
+            person.Name = "MIke";
+            person.Age = 20;
+            person.GetNameAndAge();
+
+            Teacher teacher = new Teacher();
+            teacher.Name = "Thomas";
+            teacher.Age = 35;
+            teacher.TeacherId = "190302";
+            teacher.Subject = "Math";
+            teacher.GetNameAndAge();
+
+            Students students = new Students();
+            students.Name = "Sara";
+            students.Age = 19;
+            students.StudentsId = "10506";
+            students.Email = "sara1234@amikom.ac.id";
+            students.GetNameAndAge();
+
+            Console.ReadKey();
+        }
+    }
+}
